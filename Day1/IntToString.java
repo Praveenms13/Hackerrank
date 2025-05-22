5909b6659d74d0c45ab061e8076adfb27d141fcb
Praveen <mspraveenkumar77@gmail.com>
2025-05-22 08:13:35 +0530

    Updates

---
+++ Day1/IntToString.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class IntToString {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        try {
+            String s = Integer.toString(n);
+            System.out.print("Good job");
+        } catch (Exception e) {
+            System.out.print("Wrong Answer");
+        }
+    }
+}
---
+++ Day1/Loops2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Loops2{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int sum = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            for (int j=0;j<n;j++) {
+                sum += (int) Math.pow(2, j) * b;
+                System.out.print(sum + " ");
+            } System.out.println();
+        }
+        in.close();
+    }
+}
